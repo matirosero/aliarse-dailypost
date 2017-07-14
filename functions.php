70c9a20aebe4fd76ca0a4d0808a60c656c92e4d7
MatiRosero <gekidasa@gmail.com>
2017-07-13 21:08:31 -0600

    initial files

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'dailypost_parent_theme_enqueue_styles' );
+
+function dailypost_parent_theme_enqueue_styles() {
+    wp_enqueue_style( 'dailypost-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'aliarse-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'dailypost-style' )
+    );
+
+}
