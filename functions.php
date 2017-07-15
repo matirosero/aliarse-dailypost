011e2eaf274912bb9f08de2feb3446898b88e10a
MatiRosero <gekidasa@gmail.com>
2017-07-14 18:36:55 -0600

    spanish translation

--- functions.php
+++ functions.php
@@ -6,3 +6,11 @@ include( 'includes/debug.php' );
 // Enqueue and dequeue
 include( 'includes/enqueue.php' );
 
+
+/*
+ * Load translation in child theme
+ */
+function child_theme_slug_setup() {
+    load_child_theme_textdomain( 'dailypost', get_stylesheet_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'child_theme_slug_setup' );
