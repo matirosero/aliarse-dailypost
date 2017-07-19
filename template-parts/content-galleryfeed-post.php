fa433aa4a0053e1153f3005a1b8d57e4386cb646
MatiRosero <gekidasa@gmail.com>
2017-07-19 17:22:16 -0600

    grabbed structure for gallery section

---
+++ template-parts/content-galleryfeed-post.php
@@ -0,0 +1,24 @@
+<div class="wpb_column vc_column_container vc_col-sm-6 vc_col-md-4"><div class="vc_column-inner "><div class="wpb_wrapper">
+
+	<div class="post featured-post small-featured-post ">
+
+		<div class="post-meta">
+			<h3 class="post-title"><a href="//localhost:3000/otra-galeria-mas/">Otra galería más</a></h3>
+
+			<p class="post-author">por <a href="//localhost:3000/author/webdev/" class="author">webdev</a></p>
+		</div><!-- .post-meta -->
+
+		<div class="post-cover">
+			<ul class="post-categories clean-list">
+				<li class="category"><a href="//localhost:3000/category/galeria/">Galería</a></li>
+			</ul><!-- .post-categories clean-list -->
+
+			<a href="//localhost:3000/otra-galeria-mas/">
+				<img width="528" height="320" src="//localhost:3000/wp-content/uploads/2017/07/evento_18-528x320.jpg" class="attachment-small-featured-post size-small-featured-post wp-post-image" alt="">
+			</a>
+			<span class="hover-color" style="background-color: rgba(17, 138, 195, 0.5)"></span>
+		</div><!-- .post-cover -->
+
+	</div><!-- .post featured-post small-featured-post -->
+	
+</div></div></div>
---
+++ template-parts/content-galleryfeed.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Shows gallery feed
+ */
+?>
+<section class="gallery-feed devbox" style="margin-top: 90px !important; margin-bottom: 40px !important;">
+	<div class="container-fluid clearfix ovh">
+		<div class="row">
+
+			<?php get_template_part( 'template-parts/content', 'galleryfeed-post' ); ?>
+
+		</div><!-- .row -->
+	</div><!-- .container-fluid clearfix ovh -->
+</section><!-- .gallery-feed -->
