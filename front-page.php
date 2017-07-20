acc7cefe1cbc9f0468d707586c201de0a7eab77c
MatiRosero <gekidasa@gmail.com>
2017-07-19 20:43:55 -0600

    structure for signup form

--- front-page.php
+++ front-page.php
@@ -77,6 +77,7 @@ get_header();?>
 	endif; ?>
 	<!-- </div> --><!-- Extra closing div, mistake? -->
 	<div class="" style="width:100%; background:red; margin: 50px,0;">BELOW VC CONTENT</div>
+	<?php get_template_part( 'templates/content', 'signup' ); ?>
 	<?php get_template_part( 'templates/content', 'galleryfeed' ); ?>
 	<?php get_template_part( 'templates/content', 'blogfeed' ); ?>
 <?php get_footer();?>
---
+++ templates/content-signup.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<section class="vc_section vc_custom_1500069915146">
+	<div class="container-fluid clearfix ovh">
+		<div class="row">
+			<div class="wpb_column vc_column_container vc_col-sm-6 vc_col-lg-offset-1 vc_col-lg-5">
+				<div class="vc_column-inner">
+					<div class="wpb_wrapper">
+						<div class="vc_custom_heading vc_custom_1500069963258 uppercase">
+							<h4 style=" font-weight: 500; font-size: 20px;color: #ffffff;text-align: left">Suscríbase a nuestro boletín.</h4>
+						</div><!-- .vc_custom_heading vc_custom_1500069963258 uppercase -->
+					</div><!-- .wpb_wrapper -->
+				</div><!-- .vc_column-inner -->
+			</div><!-- .wpb_column vc_column_container vc_col-sm-6 vc_col-lg-offset-1 vc_col-lg-5 -->
+			<div class="wpb_column vc_column_container vc_col-sm-6 vc_col-lg-5">
+				<div class="vc_column-inner">
+					<div class="wpb_wrapper">
+						<form class="subscribe-form alignright " id="newsletter" method="post" data-tt-subscription="">
+							<input type="text" name="email" class="form-input check-value" placeholder="Correo electrónico" data-tt-subscription-required="" data-tt-subscription-type="email">
+							<input type="submit" class="form-submit" value="Subscríbase">
+							<div class="result_container"></div>
+						</form><!-- .subscribe-form -->
+					</div><!-- .wpb_wrapper -->
+				</div><!-- .vc_column-inner -->
+			</div><!-- .wpb_column vc_column_container vc_col-sm-6 vc_col-lg-5 -->
+		</div><!-- .row -->
+	</div><!-- .container-fluid clearfix ovh -->
+</section><!-- .vc_section -->
