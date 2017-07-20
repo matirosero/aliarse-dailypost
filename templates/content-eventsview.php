5d08c206b1764af414f5a6ca5e226c4018b3f81d
MatiRosero <gekidasa@gmail.com>
2017-07-20 00:40:36 -0600

    created tempalte part for events

---
+++ templates/content-eventsview.php
@@ -0,0 +1,2 @@
+<?php
+echo do_shortcode( '[tribe_events view="list"]' );
--- templates/content-signup.php
+++ templates/content-signup.php
@@ -4,7 +4,7 @@
 <section class="vc_section vc_custom_1500069915146">
 	<div class="container-fluid clearfix ovh">
 		<div class="row">
-			<div class="wpb_column vc_column_container vc_col-sm-6 vc_col-lg-offset-1 vc_col-lg-5">
+			<div class="col-sm-6 col-lg-offset-1 col-lg-5">
 				<div class="vc_column-inner">
 					<div class="wpb_wrapper">
 						<div class="vc_custom_heading vc_custom_1500069963258 uppercase">
@@ -13,7 +13,7 @@
 					</div><!-- .wpb_wrapper -->
 				</div><!-- .vc_column-inner -->
 			</div><!-- .wpb_column vc_column_container vc_col-sm-6 vc_col-lg-offset-1 vc_col-lg-5 -->
-			<div class="wpb_column vc_column_container vc_col-sm-6 vc_col-lg-5">
+			<div class="col-sm-6 col-lg-5">
 				<div class="vc_column-inner">
 					<div class="wpb_wrapper">
 						<form class="subscribe-form alignright " id="newsletter" method="post" data-tt-subscription="">
@@ -25,6 +25,7 @@
 							*/ ?>
 							<?php echo do_shortcode( '[mc4wp_form id="546"]' ); ?>
 						</form><!-- .subscribe-form -->
+						<div class="result_container"></div>
 					</div><!-- .wpb_wrapper -->
 				</div><!-- .vc_column-inner -->
 			</div><!-- .wpb_column vc_column_container vc_col-sm-6 vc_col-lg-5 -->
