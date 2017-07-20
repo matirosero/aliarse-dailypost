dd7d80c6a6dfe0db361ba4ecc17ee1dd8c776f20
MatiRosero <gekidasa@gmail.com>
2017-07-20 14:43:02 -0600

    center column class

--- templates/content-eventsview.php
+++ templates/content-eventsview.php
@@ -1,2 +1,15 @@
 <?php
-echo do_shortcode( '[tribe_events view="list"]' );
+/*
+ * Shows events
+ */
+?>
+
+<section class="eventsview" style="margin-top: 90px !important; margin-bottom: 40px !important;">
+	<div class="container-fluid clearfix ovh">
+		<div class="row row-centered">
+			<div class="col-md-9 col-centered">
+				<?php echo do_shortcode( '[tribe_events view="list"]' ); ?>
+			</div>
+		</div><!-- .row -->
+	</div><!-- .container-fluid clearfix ovh -->
+</section><!-- .eventsview -->
